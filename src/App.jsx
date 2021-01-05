c762d969bf338ae0841d274248c396283d438693
Aaron Brown <aaroncbrown1993@gmail.com>
2021-01-04 22:00:35 -0500

    Completed portion of navbar redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavBar />
-    </div>
+    </Router>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,37 +1,18 @@
-import './NavBar.css';
-import React, { Component } from 'react';
-import { menuItems } from './MenuItems';
-import { Button } from '../Button';
+import React from 'react'
+import { Nav, NavBarContainer, NavLogo } from './NavBarElements';
 
-class NavBar extends Component {
-    state = { clicked: false };
-
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
-    };
-
-    render() {
-        return(
-            <nav className="NavBar">
-                <h1 className="nav-bar-logo">React<i className="fab fa-react"></i></h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={'nav-menu' + (this.state.clicked ?  ' active' : '')}>
-                    {menuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <Button>Sign Up</Button>
-            </nav>
-        );
-    }
+const NavBar = () => {
+    return (
+        <>
+            <Nav>
+                <NavBarContainer>
+                    <NavLogo to='/'>
+                        dolla
+                    </NavLogo>
+                </NavBarContainer>
+            </Nav>
+        </>
+    );
 }
 
-export default NavBar;
+export default NavBar
